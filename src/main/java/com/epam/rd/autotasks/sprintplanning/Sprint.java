4a48786018d5918e31eeffe3c3bc420d73f0ade5
Beata Podgorska <beata.podgorska26@gmail.com>
2022-04-27 13:11:09 +0200

    Solved

--- src/main/java/com/epam/rd/autotasks/sprintplanning/Sprint.java
+++ src/main/java/com/epam/rd/autotasks/sprintplanning/Sprint.java
@@ -4,13 +4,13 @@ import com.epam.rd.autotasks.sprintplanning.tickets.Bug;
 import com.epam.rd.autotasks.sprintplanning.tickets.Ticket;
 import com.epam.rd.autotasks.sprintplanning.tickets.UserStory;
 import java.util.ArrayList;
-import java.util.List;
 
 public class Sprint {
     private final int CAPACITY;
     private final int TICKETS_LIMIT;
     private int capacityNow = 0;
     private int numberOfTickets = 0;
+    //private List<Ticket> listsOfUserStories = new LinkedList<Ticket>();
     private Ticket[] arrayOfUserStories=new Ticket[1];
 
     public Sprint(int capacity, int ticketsLimit) {
@@ -19,35 +19,40 @@ public class Sprint {
     }
 
     public boolean addUserStory(UserStory userStory) {
+        //checking if userStory can be accepted
         if(numberOfTickets >= TICKETS_LIMIT || userStory == null || 
-        ( capacityNow + userStory.getEstimate()) > CAPACITY || userStory.isCompleted()){
+        (capacityNow + userStory.getEstimate()) > CAPACITY || userStory.isCompleted()){
             return false;
         }
         else{
-                
-            numberOfTickets++; 
-            capacityNow += checkCapacity(userStory);
-            
-            Ticket[] copy = new Ticket[numberOfTickets];
-            System.arraycopy(arrayOfUserStories, 0, copy, 0, arrayOfUserStories.length); 
-            copy[numberOfTickets-1] = userStory;
-            arrayOfUserStories = new Ticket[numberOfTickets];
-            System.arraycopy(copy, 0, arrayOfUserStories, 0, copy.length);  
-            return true; 
-            
+            //checking if userStory uncompleted dependencies are already accepted 
+            if(userStory.userStory == null) return addUser(userStory);
+            else if(arrayOfUserStories == null && !(userStory.userStory == null)) return false;
+            else{
+                int counter = 0;
+                for(int i = 0;i<userStory.userStory.length;i++){
+                    for(int j = 0;j<arrayOfUserStories.length;j++){
+                        if(arrayOfUserStories[j]==userStory.userStory[i]) counter++;
+                    }
+                    if(counter>0)counter =0;
+                    else return false;
+                }
+                return addUser(userStory);
+            }
         } 
     }
+    public boolean addUser(UserStory userStory){
+        //adding UserStory to the list Of UserStory
+        numberOfTickets++; 
+        capacityNow += userStory.getEstimate();
+            
+        Ticket[] copy = new Ticket[numberOfTickets];
+        System.arraycopy(arrayOfUserStories, 0, copy, 0, arrayOfUserStories.length); 
+        copy[numberOfTickets-1] = userStory;
+        arrayOfUserStories = new Ticket[numberOfTickets];
+        System.arraycopy(copy, 0, arrayOfUserStories, 0, copy.length);  
+        return true; 
 
-    public int checkCapacity(UserStory userStory){
-        UserStory[] users = userStory.getDependencies();
-        int capacity = userStory.getEstimate();
-        if(users.length==0){
-            return capacity;
-        }
-        for(int i = 0;i<users.length;i++){
-            capacity+= users[i].getEstimate();
-        }
-        return capacity;
     }
 
     public boolean addBug(Bug bugReport) {
@@ -56,18 +61,8 @@ public class Sprint {
             return false;
         }
         else{
-
-            
-            //    int counter = 0;
-            //    for(int i = 0;i<bugReport.userStory.userStory.length;i++){
-            //        for(int j = 0;j<arrayOfUserStories.length;j++){
-           //             if(bugReport.userStory.userStory[i].getId()==arrayOfUserStories[j].getId())counter++;
-            //        }
-            //        if(counter == 0)return false;
-            //        else counter = 0;
-            //    }
-        }
-            
+            //adding bugReport
+            if(bugReport.isCompleted())return false;
             numberOfTickets++; 
             capacityNow += bugReport.getEstimate();
         
@@ -77,17 +72,19 @@ public class Sprint {
             arrayOfUserStories = new Ticket[numberOfTickets];
             System.arraycopy(copy, 0, arrayOfUserStories, 0, copy.length);  
             return true; 
-            
-        } 
+        }
+    } 
     
     
     public Ticket[] getTickets() {
+        //return copy array of tickets
         Ticket[] copy = new Ticket[numberOfTickets];
         System.arraycopy(arrayOfUserStories, 0, copy, 0, arrayOfUserStories.length); 
         return copy;
     }
 
     public int getTotalEstimate() {
+        //return taken capacity
         return capacityNow;
     }
 }
